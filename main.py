1a189fdb69a0f752d0b40cc2ce056b552fad7cc6
LndTDB <leandrobarcelos.f@hotmail.com>
2021-04-26 13:00:30 -0300

    Added a highscore!

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import sys
 from turtle import Screen, Turtle
 from time import sleep
 from random import randint
+import json
+import turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgpic('img/bg_image.png')
 screen.title("Snake Game")
 screen.tracer(0)
+
 
 
 class Snake:
@@ -33,6 +36,14 @@ class Snake:
         new_s.pu()
         new_s.goto(self.segments[len(self.segments) - 1].pos())
         self.segments.append(new_s)
+
+    def reset_snake(self):
+        for segment in self.segments:
+            segment.reset()
+            segment.ht()
+        self.starting_position = [(0, 0), (-20, 0), (-40, 0)]
+        self.segments = []
+        self.create_snake_body()
 
     # Movement functions
     def move_up(self):
@@ -78,13 +89,7 @@ class Snake:
         if out_down_up or out_left_right:
             return True
 
-    # Ends game and prints score if any of the two function above return True
-    def lost(self, score_class):
-        if self.hit_border() or self.hit_tale():
-            print(f"You lost!\nScore: {score_class.score}")
-            sys.exit()
-
-
+    
 class Food:
     def __init__(self):
         self.food = Turtle('square')
@@ -105,12 +110,23 @@ class Scoreboard:
         self.writer.pu()
         self.writer.pencolor('black')
         self.score = score
+        with open("highscore.txt", 'r+') as file:
+            try:
+                self.highscore = int(file.read())
+            except:
+                self.highscore = 0
 
     # Write on screen how many foods the snake consumed
     def write(self):
         self.writer.goto(0, 250)
         self.writer.clear()
-        self.writer.write(f"Score: {self.score}", align='center', font=('Pixeboy', 20, 'normal'))
+        self.writer.write(f"Score: {self.score} Highscore: {self.highscore}", align='center', font=('Pixeboy', 20, 'normal'))
+
+    def reset_score(self):
+        if self.highscore < self.score:
+            self.highscore = self.score
+        self.score = 0
+        self.write()
 
 
 # Generates a random coordinate tuple
@@ -136,18 +152,24 @@ score_ = 0
 player_score = Scoreboard(score_)
 player_score.write()
 sleep(2)
-while game_is_on:
-    screen.update()
-    player.lost(player_score)
-    sleep(0.1)
-    player_score.write()
-    player_score.write()
-    if player.eat_food():
-        player_score.score += 1
-    for i in range(len(player.segments) - 1, -1, -1):
-        if i == 0:
-            player.segments[i].fd(20)
-        else:
-            player.segments[i].goto(player.segments[i - 1].pos())
+try:
+    while game_is_on:
+        screen.update()
+        sleep(0.1)
+        player_score.write()
+        player_score.write()
+        if player.eat_food():
+            player_score.score += 1
+        for i in range(len(player.segments) - 1, -1, -1):
+            if i == 0:
+                player.segments[i].fd(20)
+            else:
+                player.segments[i].goto(player.segments[i - 1].pos())
+        if player.hit_border() or player.hit_tale():
+            player.reset_snake()
+            player_score.reset_score()
+except:
+    with open("highscore.txt", 'r+') as file:
+        file.write(f"{player_score.highscore}") 
 
 screen.exitonclick()
